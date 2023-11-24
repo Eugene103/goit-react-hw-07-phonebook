a4a45171f8e6b907c1b1366ea1137b9dbaf59680
Eugene103 <chertyka74@gmail.com>
2023-11-24 16:37:50 +0200

    v2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,24 +2,8 @@ import { ContactForm } from "components/ContactForm/ContactForm";
 import { ContactsList } from "components/ContactsList/ContactsList";
 import { Container, Section } from "./GlobalStyle";
 import { FindCont } from "components/FindCont/FindCont";
-import { useDispatch, useSelector} from 'react-redux';
-import { useEffect } from "react";
-import { fetchContacts } from "redux/operations";
-
 
 export const App = () => {
-  const dispatch = useDispatch();
-
-  const contacts = useSelector(state => state.contacts.contacts);
-  const filter = useSelector(state => state.filter.filter)
-  
- const filterContacts = contacts.filter(
-    (contact) => contact.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1,
-  );
-  ;
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch])
 
   return <Section>
       <h1>PhoneBook</h1>
@@ -27,7 +11,7 @@ export const App = () => {
       <Container>
         <h2>Contacts</h2>
         <FindCont />
-      <ContactsList arr={filterContacts}/>
+      <ContactsList/>
       </Container>
     </Section>
 }
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,19 +1,29 @@
 import { Container, Item, List, Btn } from "./ContactsList.styled";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { initialFilter } from "redux/filterSlice";
-import { deleteContact } from "redux/operations";
+import { deleteContact, fetchContacts } from "redux/operations";
+import { useEffect } from "react";
 
+export const ContactsList = () => {
+const dispatch = useDispatch();
+const contacts = useSelector(state => state.contacts.contacts);
+const filter = useSelector(state => state.filter.filter)
 
-export const ContactsList = ({ arr}) => {
-    const dispatch = useDispatch();
-
+useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch])
+    
+ const filterContacts = contacts.filter(
+    (contact) => contact.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1,
+  );
+  ;
     const removeContact = (id) => [
         dispatch(deleteContact(id)),
         dispatch(initialFilter())
   ]
     return <Container>
         <List>
-            {arr.map(({id, name, number}) => (
+            {filterContacts.map(({id, name, number}) => (
                 <Item key={id}>
                     <p>{name}:</p><p>{number}</p>
                 <Btn id={id} onClick={evt => removeContact(evt.target.id)}>Delete</Btn>
